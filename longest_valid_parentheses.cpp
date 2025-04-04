7a73f83a4e619cfc3a783f99af2b5ff4bd319f19
KHUSHI NAGAR <155895880+khushinagar01@users.noreply.github.com>
2025-04-04 16:49:49 +0530

    Add files via upload

---
+++ longest_valid_parentheses.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int longestValidParentheses(string s) 
+{
+    stack<int> st;
+    st.push(-1);
+    int maxLen = 0;
+    for (int i = 0; i < s.length(); i++) 
+    {
+        char ch = s[i];
+        if (ch == '(') 
+        {
+            st.push(i);
+        } 
+        else 
+        {
+            st.pop();
+            if (st.empty()) 
+            {
+                st.push(i);
+            } 
+            else 
+            {
+                int len = i - st.top();
+                maxLen = max(maxLen, len);
+            }
+        }
+    }
+    return maxLen;
+}
+
+int main() 
+{
+    string input;
+    cout << "Enter a string of parentheses: ";
+    cin >> input;
+
+    int result = longestValidParentheses(input);
+    cout << "Longest valid parentheses length: " << result << endl;
+
+    return 0;
+}
